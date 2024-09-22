533240c3153e6efa028a1531aa3655171dda552b
Kame-Bashuna <basshunakameroba@gmail.com>
2024-09-22 22:44:16 +0300

    added changes

--- bheta_solution/settings.py
+++ bheta_solution/settings.py
@@ -152,8 +152,3 @@ if ENV_FILE:
 
 
 
-AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN","")
-AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID","")
-AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET","")
-REDIRECT_URI=os.environ.get("REDIRECT_URI","")
-
