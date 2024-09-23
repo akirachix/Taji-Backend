dabe9d280cd6e54abcb022346d58e5e5421173db
Kame-Bashuna <basshunakameroba@gmail.com>
2024-09-23 13:01:43 +0300

    added workflow

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ from .pharmacy_views import get_nearby_pharmacies
 
 urlpatterns = [
     path('image-upload/', ImageUploadView.as_view(), name='image-upload'),
-    \
+
     path('pharmacies/', PharmacyListView.as_view(), name='pharmacy-list'),
     path('pharmacies/<int:id>/', PharmacyDetailView.as_view(), name='pharmacy-detail'),
     path('pharmacies/nearby/', get_nearby_pharmacies, name='get-nearby-pharmacies'),
